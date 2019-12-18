fac6d314400cec48d9250ff31796adfb386be3ea
Jevon Cochran <jevon@Jevons-MacBook-Pro.local>
2019-12-18 15:08:49 -0300

    lost

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,12 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Dashboard from "./Dashboard";
+
 // Test away
+test('Dashboard renders correctly', () => {
+    render(<Dashboard />);
+})
+
+test('defaults to open and unlocked', () => {
+    expect(render(<Dashboard />)).toMatchSnapshot();
+})
